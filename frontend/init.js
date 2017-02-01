a97c3b0fe76868839afe17951f10583c0d700072
Dan Metzler <dmetzler@ucsd.edu>
2017-01-31 16:24:53 -0800

    Completed first few pages

---
+++ frontend/init.js
@@ -0,0 +1,3 @@
+// add click listeners to homescreen buttons
+document.getElementById("splash-login").addEventListener("click", beginLogin);
+document.getElementById("splash-signup").addEventListener("click", beginSignup);
--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,31 @@
+// begin login process
+function beginLogin() {
+    // TODO
+}
+
+// begin signup process
+function beginSignup() {
+    console.log("Loading signup");
+    loadScreen("screen-signup");
+}
+
+// loads a given screen
+function loadScreen(id) {
+    $(".active-screen").css({
+        top: 0,
+        left: "-100vw"
+    });
+    setTimeout(function() {
+        $(".screen-wrapper").removeClass("active-screen");
+        $("#" + id).addClass("active-screen");
+        setTimeout(function() {
+            $("#" + id).css({
+                top: 0,
+                left: 0
+            });
+        }, 0);
+    }, 0);
+}
+
 // set Stripe API key
 Stripe.setPublishableKey(config.STRIPE_KEY);
